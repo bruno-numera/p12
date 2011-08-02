6a96f015f966896c9fb38ca100905fd0bd0870f6
Rafael Seemann <rseemann@gmail.com>
2011-08-02 18:11:05 -0300

    Rearranjando os arquivos do plugin do firefox e arrumando os nomes dos arquivos

---
+++ plugins/firefox/p12/content/overlay.js
@@ -0,0 +1,10 @@
+(function(){
+	var body = $("body");	
+	var p12Arrow = $("<div id='p12_arrow'></div>");
+	body.after(p12Arrow);
+	p12Arrow.click(function(){
+		body.toggleClass("rotated");
+	});	
+	
+	alert('opa!');
+}());
