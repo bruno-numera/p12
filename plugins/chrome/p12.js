b80934cb170504273e5379bd5677d014f30f608b
Lucas <barrozo.lucas@gmail.com>
2011-08-02 18:14:10 -0300

    Atualizado plugin chrome.

--- plugins/chrome/p12.js
+++ plugins/chrome/p12.js
@@ -1,7 +1,7 @@
 (function(){
 	var body = $("body");	
 	var p12Arrow = $("<div id='p12_arrow'></div>");
-	body.after(p12Arrow);
+	body.append(p12Arrow);
 	p12Arrow.click(function(){
 		body.toggleClass("rotated");
 	});	
