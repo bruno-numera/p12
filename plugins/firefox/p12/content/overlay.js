b8c8514107fb33d9c586503530d883629d2dda32
Rafael Seemann <rseemann@gmail.com>
2011-08-03 15:24:30 -0300

    Escutando evento quando a página está carregada, no entanto o callback é chamado duas vezes.

--- plugins/firefox/p12/content/overlay.js
+++ plugins/firefox/p12/content/overlay.js
@@ -1,10 +1,31 @@
-(function(){
-	var body = $("body");	
-	var p12Arrow = $("<div id='p12_arrow'></div>");
-	body.after(p12Arrow);
-	p12Arrow.click(function(){
-		body.toggleClass("rotated");
-	});	
-	
-	alert('opa!');
-}());
+window.addEventListener("load", function() { 
+	var appcontent = document.getElementById("appcontent");   // browser
+	if(appcontent)
+		alert('adding listener');
+		appcontent.addEventListener("DOMContentLoaded", function(){
+				alert('DOM loaded');
+		}, false);
+}, false);
+
+/*
+var myExtension = {
+		init: function() {
+			,
+
+			onPageLoad: function(aEvent) {
+				var doc = aEvent.originalTarget; // doc is document that triggered "onload" event
+				// do something with the loaded page.
+				// doc.location is a Location object (see below for a link).
+				// You can use it to make your code executed on certain pages only.
+				if(doc.location.href.search("forum") > -1)
+					alert("a forum page is loaded");
+
+				// add event listener for page unload 
+				aEvent.originalTarget.defaultView.addEventListener("unload", function(event){ myExtension.onPageUnload(event); }, true);
+			},
+
+			onPageUnload: function(aEvent) {
+				// do something
+			}
+		}
+*/
