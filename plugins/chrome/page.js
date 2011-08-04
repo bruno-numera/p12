27bf0df5322898dd213f06255a442f1d4b2818fc
Lucas <barrozo.lucas@gmail.com>
2011-08-04 16:57:52 -0300

    Plugin chrome em 80%

--- plugins/chrome/page.js
+++ plugins/chrome/page.js
@@ -1,27 +1,28 @@
-(function(){	
-	var arrow = $("<div id='p12Arrow'></div>").appendTo($("body")).click(function(){
-		$(this).remove();
-		chrome.extension.sendRequest(null, function(response){		
-			$("body").html("<img src='"+response.dataUrl+"'/>");
-		});
-	});
-	
-	/*	
-		  var body = $("body");
-		  
-		  var container = $("<div class='p12container'></div>").appendTo(body);
-		  
-		  var card = $("<div class='card'></div>").appendTo(container); 
-		  var front =  $("<div class='front figure'></div>").appendTo(card);
-		  var back = $("<div class='back figure'></div>").appendTo(card);
-		  
-		  front.append("<span style='font-size:200px;'>1</span>");
-		  back.append("<span style='font-size:200px;'>2</span>");
-		  
-		  card.click(function(){ $(this).toggleClass("flipped"); });
-		  
-	 */
-		
-	
-})();
+(function() {
 
+	$("<div id='p12Arrow'></div>").appendTo($("body")).click(function() {
+				$(this).remove();				
+				chrome.extension.sendRequest(null, function(response) {	
+					var body = $("body");
+					
+					
+					var container = $("<div class='p12container'></div>");
+					body.after(container);
+					
+					var card = $("<div class='card'></div>").appendTo(container);
+					
+					var front = $("<div class='front figure'></div>").appendTo(card);
+					front.append("<img src='" + response.dataUrl + "'/>");
+						
+					var back = $("<div class='back figure'></div>").appendTo(card);
+
+					back.append("<span style='font-size:200px;'>2</span>");
+					
+					body.empty();
+					
+					setTimeout(function(){
+						card.toggleClass("flipped");
+					},300)
+				});
+			});
+})();
